27f79b6431488fda3d8a2281d781a6c7a2db62d1
hoffi <hoffi@BlackGalaxy>
2017-10-17 11:36:58 +0100

    initial

---
+++ src/main/java/cz/hoffic/hoffiland/Hoffiland.java
@@ -0,0 +1,21 @@
+package cz.hoffic.hoffiland;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Hoffiland.MODID, version = Hoffiland.VERSION)
+public class Hoffiland
+{
+    public static final String MODID = "hoffiland";
+    public static final String VERSION = "0.0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+        System.out.println("YOLO");
+    }
+}
